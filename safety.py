d4d3a4947d5dfa402aee9487d759cfed67b85bee
ViViD <vivnet@comcast.net>
2022-03-31 20:12:03 -0700

    Add files via upload

---
+++ safety.py
@@ -0,0 +1,42 @@
+#
+##########
+#
+#   Simple program to protect against ovherheat
+#
+#   Set username to whatever user has the crypto pedal through the floor
+#
+#   Run as a different user with privilege to kill
+#
+#########
+#
+
+
+import os
+import time
+import psutil
+
+
+print("----------")
+
+
+while True:
+    
+    username = ""
+    
+    temp = psutil.sensors_temperatures()
+    cputemp = []
+    for name, entries in temp.items():
+        for entry in entries:
+            cputemp.append(entry.current)
+    hightemp = max(cputemp)
+    print("Temp: " + str(hightemp) + "C")
+
+    if hightemp >= 85:
+        for proc in psutil.process_iter():
+            if proc.username() == username:
+                print("Killing process " + str(proc.pid))
+                os.kill(proc.pid, 9)
+    
+    print("----------")
+    time.sleep(5)
+    
